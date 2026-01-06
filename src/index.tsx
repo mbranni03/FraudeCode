5de7f7a80d731b707934836c0cf21a961100ccf9
Matthew Branning <48932625+juandap1@users.noreply.github.com>
2026-01-05 20:41:41 -0500

    added settings

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,14 @@
 import { render } from "ink";
 import App from "./components/App";
 import { resetLog } from "./utils/logger";
+import { Settings, UpdateSettings } from "./utils/Settings";
 
-resetLog();
-console.clear();
-render(<App />);
+async function main() {
+  resetLog();
+  console.clear();
+  await Settings.init();
+  UpdateSettings("lastOpened", new Date().toISOString());
+  render(<App />);
+}
+
+main();
